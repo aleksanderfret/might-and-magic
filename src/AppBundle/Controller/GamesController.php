a1bdda4fd5e74ad6408052b2de98798e4299a659
Olek <Olek@192.168.1.5>
2017-02-20 17:54:42 +0100

    symfony po lekcji 10 koniec kursu podstawowego

--- src/AppBundle/Controller/GamesController.php
+++ src/AppBundle/Controller/GamesController.php
@@ -4,8 +4,10 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GamesController extends Controller{
@@ -15,12 +17,26 @@ class GamesController extends Controller{
      */    
     public function showAction($gameTitle){
         
-        $templating = $this->container->get('templating');
+        #$templating = $this->container->get('templating');
         
-        $html = $templating->render('gry/show.html.twig', ['name'=> $gameTitle]);
+        return $this->render('gry/show.html.twig', ['name'=> $gameTitle]);
+    }
+    
+    /**
+     * @Route("/gry/{gameTitle}/notes", name="gry_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($gameTitle){
+         $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],];
         
-        return new Response($html);
+        $data = ['notes' => $notes];
         
+        #return new Response(json_encode($data));
+        
+        return new JsonResponse($data);
     }
 }
 
