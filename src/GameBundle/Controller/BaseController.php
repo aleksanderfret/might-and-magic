fc587db017e8819f8cf7b2770f253bf80957d5da
Olek <Olek@192.168.1.5>
2017-05-04 23:26:38 +0200

    logout listener delete BaseController clean for future features for example different responses for ajax request

--- src/GameBundle/Controller/BaseController.php
+++ src/GameBundle/Controller/BaseController.php
@@ -6,15 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BaseController extends Controller
 {
-    private $entityMenager;
     
-    public function __construct()
-    {
-        $this->entityMenager = $this->get('doctrine')->getManager();
-    }
-
-    public function getEntityManager()
-    {
-        return $this->entityManager;
-    }
 }
